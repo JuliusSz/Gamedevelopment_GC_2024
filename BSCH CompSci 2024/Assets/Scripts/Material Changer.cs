cf0069dd62101455c91cc62446b045ca9d0b56ff
JuliusSz <112384735+JuliusSz@users.noreply.github.com>
2024-02-13 16:52:43 +0000

    Colour Changer

---
+++ BSCH CompSci 2024/Assets/Scripts/Material Changer.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    public Material testMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testMat = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)  //Changes Colour on Colide
+    {
+        testMat.color = UnityEngine.Random.ColorHSV();
+    }
+}
